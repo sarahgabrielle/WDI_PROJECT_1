6cdcb958049c7158e3015f2b3850e72903480fb0
sarahalpay <sarah.g.alpay@gmail.com>
2017-10-09 23:46:53 +0100

    basic collition function working

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ $(() =>{
 
   function moveSpaceship(){
     $(document).keydown(function(e) {
-      var position = $('#spaceship').position();
+      const shipPosition = $('#spaceship').position();
 
       switch(e.keyCode) {
         case 37: // left
-          if(position.left > 0 ) {
+          if(shipPosition.left > 0 ) {
             $('#spaceship').css('left', '-=10px');
           }
           break;
         case 39: // right
-          if(position.left < 580 ) {
+          if(shipPosition.left < 580 ) {
             $('#spaceship').css('left', '+=10px');
           }
       }
@@ -37,17 +37,31 @@ $(() =>{
 
     function step() {
       const position = $(this).position();
-      console.log(position);
-      // Is the position of the fall element in collision with the paddle-thing
-      var ship = {x: 600, y: 0, width: 20, height: 20};
-      var star = {x: 0, y: 600, width: 20, height: 20};
+      // console.log(position);
 
-      if (ship.x < star.x + star.width &&
-   ship.x + ship.width > star.x &&
-   ship.y < star.y + star.height &&
-   ship.height + ship.y > star.y) {
-        console.log('collision detected!');
+      function collisions($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+        return true;
       }
+
+      setInterval(function(){
+        if(collisions($('.planets'), $('#spaceship'))){
+          alert('collision detected');
+        }
+      },25);
 
     }
 
