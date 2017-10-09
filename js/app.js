bb1a394d36412420f8360d1a7d719c69cc03c0ba
sarahalpay <sarah.g.alpay@gmail.com>
2017-10-09 12:24:34 +0100

    my character is moving left and right

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('loaded');
+$(() =>{
+
+  $(document).keydown(function(e) {
+    var position = $('#spaceship').position();
+
+    switch(e.keyCode) {
+      case 37: // left
+        if ( position.left > 0 ) {
+          $('#spaceship').css('left', '-=10px');
+        }
+        break;
+      case 39: // right
+        if( position.left < 580 ) {
+          $('#spaceship').css('left', '+=10px');
+        }
+    }
+  });
+
+});
