c14010089f4414ce49a4896f7f8e7e08dc183da5
sarahalpay <sarah.g.alpay@gmail.com>
2017-10-09 14:59:49 +0100

    falling object working'

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 $(() =>{
 
+  // setInterval(fallingPlanets, 500);
+
   $(document).keydown(function(e) {
     var position = $('#spaceship').position();
 
     switch(e.keyCode) {
       case 37: // left
-        if ( position.left > 0 ) {
+        if(position.left > 0 ) {
           $('#spaceship').css('left', '-=10px');
         }
         break;
       case 39: // right
-        if( position.left < 580 ) {
+        if(position.left < 580 ) {
           $('#spaceship').css('left', '+=10px');
         }
     }
   });
+
+  function fallingPlanets(){
+    $('#planets').animate({'top': '+=30px', duration: 0.1}, 500, 'linear');
+  }
+
+
 
 });
