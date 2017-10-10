1673beb18e6e32b39d83c01927e8c6046a3975c4
sarahalpay <sarah.g.alpay@gmail.com>
2017-10-10 12:02:22 +0100

    collisions logic complete within createPlanets function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 $(() =>{
 
-  moveSpaceship();
-  setInterval(createPlanets, 1000);
+  function play(){
+    moveSpaceship();
+    setInterval(createPlanets, 1000);
+  }
+
+  // createPlanets();
+
+
 
   function moveSpaceship(){
     $(document).keydown(function(e) {
@@ -20,56 +26,53 @@ $(() =>{
       }
     });
   }
-
   function createPlanets(){
     const $container = $('.container');
+
+    const randomTop = Math.floor(Math.random() * 580) + 1;
+    const randomHeight = Math.floor(Math.random() * 20) + 10;
+    const randomWidth = Math.floor(Math.random() * 10) + 10;
+
     const $planet = $('<div class="planets"></div>');
-    $planet.css('left', Math.floor(Math.random() * (600 - 20))) + 1;
+    $planet.css({'left': randomTop, 'height': randomHeight, 'width': randomWidth});
     $container.append($planet);
     $planet.animate({
       'top': '600px',
       duration: 1
     }, {
-      duration: 10000,
+      duration: 8000,
       step: step,
       complete: callback
     });
 
-    function step() {
-      const position = $(this).position();
-      // console.log(position);
+    function collisions($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = $div1.outerHeight(true);
+      var w1 = $div1.outerWidth(true);
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      var h2 = $div2.outerHeight(true);
+      var w2 = $div2.outerWidth(true);
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
 
-      function collisions($div1, $div2) {
-        var x1 = $div1.offset().left;
-        var y1 = $div1.offset().top;
-        var h1 = $div1.outerHeight(true);
-        var w1 = $div1.outerWidth(true);
-        var b1 = y1 + h1;
-        var r1 = x1 + w1;
-        var x2 = $div2.offset().left;
-        var y2 = $div2.offset().top;
-        var h2 = $div2.outerHeight(true);
-        var w2 = $div2.outerWidth(true);
-        var b2 = y2 + h2;
-        var r2 = x2 + w2;
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+      return true;
+    }
 
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-        return true;
+    function step(){
+      if (collisions($('#spaceship'), $('.planets'))) {
+        console.log('game over');
       }
-
-      setInterval(function(){
-        if(collisions($('.planets'), $('#spaceship'))){
-          alert('collision detected');
-        }
-      },25);
-
     }
 
     function callback() {
       $(this).remove();
     }
   }
-
 
 
 });
